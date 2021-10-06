07b1c933502bcdf6160fd06284d71a8f4bc935fe
Jopie2016 <jpl212011@gmail.com>
2021-10-06 16:05:02 -0400

    Python practice updated

--- pythonProject1/oop.py
+++ pythonProject1/oop.py
@@ -1,7 +1,8 @@
 class Person:
-    def __init__(self, name, age):  # Two properties of this class is name and age
+    def __init__(self, name, age, weight):  # Two properties of this class is name and age
         self.name = name
         self.age = age
+        self.weight = weight
 
     def walk(self):  # Self allows us to get access to the properties above
         print(self.name + " Is walking...")
@@ -9,15 +10,20 @@ class Person:
     def speak(self):
         print("Hello my name is " + self.name + " and I am " + str(self.age) + " years old")
 
+    def workout(self):
+        print("My goal weight is " + str(self.weight) + " lbs")
 
-john = Person("John", 22)
+
+john = Person("John", 22, 205)
 john.speak()
 john.walk()
+john.workout()
 
 
 print("------------------------------")
 
 
-bob = Person("Bob", 29)
+bob = Person("Bob", 29, 200)
 bob.speak()
 bob.walk()
+bob.workout()
